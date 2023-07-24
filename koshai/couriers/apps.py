3f50e9fd9fee5bb60a8e0e4ade7a44f4319294af
rubyazz <yersultan.abduvalov@beksar.kz>
2023-07-24 19:13:15 +0600

    pre-commit

--- koshai/couriers/apps.py
+++ koshai/couriers/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CouriersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'couriers'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "couriers"
--- koshai/koshai/views.py
+++ koshai/koshai/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
 
+
 def main(request):
     return JsonResponse({"Hello": "world"})
