d2caba50f521dfb8e82b40109bb6705ede0a1ef4
rubyazz <yersultan.abduvalov@beksar.kz>
2023-07-24 18:23:19 +0600

    fixing pre-commit

--- koshai/koshai/urls.py
+++ koshai/koshai/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "",
+    ),
 ]
--- koshai/users/models.py
+++ koshai/users/models.py
@@ -1,5 +1,10 @@
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
-                                        Group, Permission, PermissionsMixin)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    Group,
+    Permission,
+    PermissionsMixin,
+)
 from django.db import models
 
 
