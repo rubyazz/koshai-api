d290348f5a1cb9845535d4bdf59e1637da2d21e8
rubyazz <yersultan.abduvalov@beksar.kz>
2023-08-17 12:25:51 +0600

    main url

--- koshai/koshai/urls.py
+++ koshai/koshai/urls.py
@@ -13,7 +13,7 @@ schema_view = get_swagger_view(title="Koshai API")
 urlpatterns = [
     path("grappelli/", include("grappelli.urls")),  # grappelli URLS
     path("admin/", admin.site.urls),
-    path("main", views.main, name="main"),
+    path("main/", views.main, name="main"),
     path("", TemplateView.as_view(template_name="pages/index.html"), name="home"),
 
     path("api/", include("api.routers")),
