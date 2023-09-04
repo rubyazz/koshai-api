943be4df0d8c8ba1ab7468e91dbaafb6c0599141
rubyazz <yersultan.abduvalov@beksar.kz>
2023-09-04 14:19:24 +0600

    Django Environ

--- koshai/koshai/settings.py
+++ koshai/koshai/settings.py
@@ -2,18 +2,24 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t!wk!$-a43ei)41zh^97g6umz$r&s(4yys3ic4jy219x+9tw^="
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
+
+SECRET_KEY = env("SECRET_KEY")
 
 ALLOWED_HOSTS = []
 
